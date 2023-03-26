39e7a8512aa1bc89e829661c246c6e1dc0d5104a
Epicboy <minecraftisawsome007+github@gmail.com>
2023-03-26 01:24:35 +0000

    Using handlebars for template engine

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const path = require('path');
-const server = express();
+const hbs = require('hbs');
 const logger = require('./logger.js');
 const db = require('./database.js');
 const cache = require('./cacheManager.js');
+
+hbs.registerPartials(__dirname + '/views/partials', function(err) { });
+
+const server = express();
 
 // Response Codes
 const BAD_REQUEST = 400;
@@ -18,78 +22,73 @@ const ONLINE = "ONLINE";
 const authToken = process.env.AUTH_TOKEN;
 
 server.use(express.json());
+server.set('view engine', 'hbs');
 
-server.get('/', (req, res)=>{
-  res.send("<head><link rel='icon' type='image/png' href='./GoLive.png'></head><div style='display:flex;align-content:center'><img style='height:10vh;margin-right:5px' src='./GoLive.png' /><div style='display:inline-block'><h1 style='margin-bottom:0px'>GoLive</h1><h5 style='margin-top:0px'>by ericw9079</h5></div></div><iframe style='border:none;width:90vw;height:70vh' src='./live'>");
+server.get('/', (req, res) => {
+  res.render('index');
 });
 
-server.all("/GoLive.png", (req, res)=>{
-  res.sendFile("./GoLive.png",{
-        root: path.join(__dirname)
-    });
+server.all("/GoLive.png", (req, res) => {
+  res.sendFile("./GoLive.png", {
+    root: path.join(__dirname)
+  });
 });
 
-server.get('/live', async (req, res) =>{
+server.get('/live', async (req, res) => {
   let keys = await db.list("Live");
-  let online = "";
-  let offline = "";
-  for(let key in keys){
+  const channels = { online: [], offline: [] };
+  for (let key in keys) {
     let status = await db.get(keys[key]);
     let name = await cache.name(keys[key].substr(5));
-    if(status == ONLINE){
-      online += `<a rel='external' target='_blank' href='https://twitch.tv/${name}'>${name}</a><br />`;
+    if (status == ONLINE) {
+      channels.online.push(name);
     }
-    else{
-      offline += `${name}<br />`;
+    else {
+      channels.offline.push(name);
     }
   }
-  s = "<table style='width:100%'>";
-  s += "<tr><td><b>Online:</b></td><td><b>Offline:</b></td></tr>";
-  s += `<tr><td>${online}</td><td>${offline}</td></tr>`;
-  s += "</table>";
-  s += "<script>setTimeout(function(){document.location.reload()}, 5000);</script>";
-	res.set("Cache-Control", "no-cache");
-  res.send(s);
+  res.set("Cache-Control", "no-cache");
+  res.render('live', { channels });
 });
 
 server.put('/status/:uid', async (req, res) => {
-  if(req.get('X-auth-token') !== authToken) {
+  if (req.get('X-auth-token') !== authToken) {
     res.status(UNAUTHORIZED).send({});
     return;
   }
   const uid = req.params.uid;
   const status = req.body.status;
   const name = req.body.name;
-  if(!(uid > 0)){
-    res.status(BAD_REQUEST).send({error: "Invalid uid"});
+  if (!(uid > 0)) {
+    res.status(BAD_REQUEST).send({ error: "Invalid uid" });
     return;
   }
-  if(!name.match(/[a-z_0-9]{4,25}/i)) {
-    res.status(BAD_REQUEST).send({error: "Invalid Channel name"});
+  if (!name.match(/[a-z_0-9]{4,25}/i)) {
+    res.status(BAD_REQUEST).send({ error: "Invalid Channel name" });
     return;
   }
-  if(status !== ONLINE && status !== OFFLINE) {
-    res.status(BAD_REQUEST).send({error: "Invalid Status"});
+  if (status !== ONLINE && status !== OFFLINE) {
+    res.status(BAD_REQUEST).send({ error: "Invalid Status" });
     return;
   }
   try {
-    await cache.update(uid,name);
-    await db.set(`Live:${uid}`,status);
+    await cache.update(uid, name);
+    await db.set(`Live:${uid}`, status);
     res.status(OK).send();
   }
   catch (error) {
-    res.status(SERVER_ERROR).send({error});
+    res.status(SERVER_ERROR).send({ error });
   }
 });
 
 server.delete('/status/:uid', async (req, res) => {
-  if(req.get('X-auth-token') !== authToken) {
+  if (req.get('X-auth-token') !== authToken) {
     res.status(UNAUTHORIZED).send();
     return;
   }
   const uid = req.params.uid;
-  if(!(uid > 0)){
-    res.status(BAD_REQUEST).send({error: "Invalid uid"});
+  if (!(uid > 0)) {
+    res.status(BAD_REQUEST).send({ error: "Invalid uid" });
     return;
   }
   try {
@@ -98,12 +97,12 @@ server.delete('/status/:uid', async (req, res) => {
     res.status(OK).send();
   }
   catch (error) {
-    res.status(SERVER_ERROR).send({error});
+    res.status(SERVER_ERROR).send({ error });
   }
 });
 
-function keepAlive(){
-  server.listen(3000, ()=>{logger.log("Server is Ready!")});
+function keepAlive() {
+  server.listen(3000, () => { logger.log("Server is Ready!") });
 }
 
 module.exports = keepAlive;
